508df807e800e24e3c512e433a13c96540552566
DebbieElabonga <debbie.elabonga@student.moringaschool.com>
2021-07-17 12:56:28 +0300

    updated models

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime as dt
 # Create your models here.
 
 class Profile(models.Model):
@@ -11,14 +12,34 @@ class Profile(models.Model):
     def __str__(self):
          return self.user.username
 
+    @classmethod
+    def filter_by_id(cls, id):
+        profile = Profile.objects.filter(user = id).first()
+        return profile
+
 class Project(models.Model):
     project_name = models.CharField(max_length=155)
     link = models.URLField(max_length=255)
     details = models.TextField(max_length=255)
-    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="projects")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="projects")
     date = models.DateTimeField(auto_now_add=True, blank=True)
     image = models.ImageField(upload_to='projectpics/',default='a.png')
+     
+    class Meta:
+        ordering = ["-pk"]
+
+    @classmethod
+    def get_projects(cls):
+        projects = Project.objects.all()
+        return projects
 
     def __str__(self):
         return self.project_name
 
+    def save_project(self):
+        self.save()
+    @classmethod
+    def search_by_project_name(cls,search):
+    	projects = cls.objects.filter(project_name__icontains=search)
+    	return projects
+    
